3155bf61fbaee8281cb0916171324bc3d85fdd68
Ariven <ariven@Arivens-Mac.local>
2017-09-03 11:19:27 +0530

    lesson03 - Playing Audio file

--- RetroCalculator/ViewController.swift
+++ RetroCalculator/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav")
+        let soundURL = URL(fileURLWithPath: path!)
+        
+        do{
+        
+            try player = AVAudioPlayer(contentsOf: soundURL)
+            player.prepareToPlay()
+        }
+        catch let err as NSError{
+        
+            print(err.debugDescription)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func numberPressed(sender: UIButton){
+    
+        playSound()
     }
 
-
+    func playSound(){
+    
+        if player.isPlaying{
+            
+            player.stop()
+        }
+        player.play()
+    }
 }
 
